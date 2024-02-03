7d64ed4214902c6c6ddf306da3e0b1877acd074b
Tony <109382836+peppacaiou@users.noreply.github.com>
2024-02-02 17:39:17 -0800

    created database schema (#12)
    * created database schema
    
    * installed
    
    * installed pt2
    
    * fixed
    
    * migration

---
+++ src/app/database/schema.ts
@@ -0,0 +1,19 @@
+export type user = {
+    "name":string,
+    "email" : string,
+    "image":string,
+    "classes": Class[],
+    "unavailableTimes":string[]
+}
+interface Class {
+    className : string,
+    classID : string,
+    startTime : Time,
+    endTime: Time,
+    professor: string,
+
+}
+interface Time{
+    hour:number
+    minute:number
+}
