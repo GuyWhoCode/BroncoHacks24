e2ad14be060690d5c238889155f65c4fb3792de3
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2024-02-02 17:19:52 -0800

    fix: extracted out options to separate file

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,6 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+import authOptions from "./options";
 
-export const authOptions: NextAuthOptions = {
-    secret: process.env.NEXTAUTH_SECRET || "",
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-        }),
-    ],
-};
 export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
