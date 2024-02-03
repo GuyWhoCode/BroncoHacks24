c8393fd11c371c7dfff8915e0777c7cfa56f0439
Jason <34526187+GuyWhoCode@users.noreply.github.com>
2024-02-02 18:03:35 -0800

    Auth (#11)
    * chore: init auth
    
    * feat: prettier
    
    * feat: render user profile
    
    * fix: extracted out options to separate file
    
    * fix: import errors
    
    * fix: type error
    
    * fix: dependencies
    
    * fix
    
    * fix
    
    * fix

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,14 @@
+import { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET || "",
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+        }),
+    ],
+};
+
+export default authOptions;
