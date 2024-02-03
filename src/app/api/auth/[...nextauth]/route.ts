a518e907d5d0e36aa4defc29ac93b0706645e6d2
GuyWhoCode <34526187+GuyWhoCode@users.noreply.github.com>
2024-02-02 17:13:37 -0800

    feat: render user profile

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,15 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET || "",
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
             clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
         }),
     ],
-});
+};
+export const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
