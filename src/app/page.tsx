fb411f84d9aa86d4173d2cd2d2a0f885a801dcf0
alimomennasab <alimomennasab3@gmail.com>
2024-02-02 22:38:31 -0800

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 import Image from "next/image";
 import { Session } from "next-auth";
 import SocialMediaCard from "./match-results";
+import ResponsiveAppBar from "./navbar";
 
 interface user {
     name: string;
@@ -12,33 +13,40 @@ interface user {
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
-    const { name = "", email = "", image = "" } = session?.user || {name: "", email: "", image: ""};
+    const { name = "", email = "", image = "" } = session?.user || {
+        name: "",
+        email: "",
+        image: "",
+    };
 
     const IMAGE_SIZE = 200;
 
     return (
-        <main>
-            {session && (
-                <>
-                    <p>
-                        User logged in: {name} <br />
-                        Email: {email}
-                    </p>
-                    <Image
-                        src={image || ""}
-                        width={IMAGE_SIZE}
-                        height={IMAGE_SIZE}
-                        alt={name || "User Image"}
-                    />
-                </>
-            )}
+        <>
+            <ResponsiveAppBar />
+            <main>
+                {session && (
+                    <>
+                        <p>
+                            User logged in: {name} <br />
+                            Email: {email}
+                        </p>
+                        <Image
+                            src={image || ""}
+                            width={IMAGE_SIZE}
+                            height={IMAGE_SIZE}
+                            alt={name || "User Image"}
+                        />
+                    </>
+                )}
 
-            <h1>BroncoHacks</h1>
-            <a href="/api/auth/signin">Login</a>
-            <br />
-            <a href="/api/auth/signout">Sign out</a>
-            <br />
-            <SocialMediaCard />
-        </main>
+                <h1>BroncoHacks</h1>
+                <a href="/api/auth/signin">Login</a>
+                <br />
+                <a href="/api/auth/signout">Sign out</a>
+                <br />
+                <SocialMediaCard />
+            </main>
+        </>
     );
 }
